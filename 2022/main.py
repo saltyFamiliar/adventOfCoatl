4c6be1e64c8ccc44c4ad773610bac4f50b72631c
cs <watchmaker3896@gmail.com>
2024-12-06 13:47:53 -0600

    moved general advent of code repo to this python-only repo

---
+++ 2022/main.py
@@ -0,0 +1,12 @@
+# import testing
+# from fifteen import fifteen
+
+# DAY = "fifteen"
+
+# print("PART ONE:\n-------------")
+# if testing.test_part(fifteen.part_one, DAY, 26):
+#     print("\nPART TWO:\n-------------")
+#     testing.test_part(fifteen.part_two, DAY, 56000011)
+
+print("Hello world")
+print("kept going here")
